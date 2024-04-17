1f1341345f870545992eb47b369ca51504522c2e
Job Guldemeester <job@dutchcodingcompany.com>
2024-04-17 08:30:18 +0200

    :bug: Added library dart doc. And rename example tests so they are also include.

--- lib/parameterized_test.dart
+++ lib/parameterized_test.dart
@@ -1,3 +1,6 @@
+/// A library that simplifies writing parameterized tests in Dart.
+library parameterized_test;
+
 export '/src/parameterized_test.dart'
     show parameterizedGroup, parameterizedTest;
 export '/src/test_options/test_options_ext.dart';
